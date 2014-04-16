a6d2bb949a65857c8e677cf66a54e483dead49bc
Taylor Tsuboi <taylort92@gmail.com>
2014-04-16 02:29:25 -0400

    equipment email notification, bugfixes

--- RoomSchedulerApp/equip.py
+++ RoomSchedulerApp/equip.py
@@ -1,6 +1,6 @@
 from google.appengine.ext import db
 import webapp2
-from google.appengine.api import users
+from google.appengine.api import users, mail
 
 import datetime
 
@@ -9,6 +9,7 @@ from models import *
 import re
 from datetime import date
 import datetime
+import logging
 
 class EquipHandler(BaseHandler):
   def get(self):
@@ -47,13 +48,20 @@ class EquipHandler(BaseHandler):
         }
         self.render_template("roomfailure.html", **template_args)
         return
-    equip = self.request.get('equiptoselect')
+    equip = self.request.get('equipmenttoselect')
     iclick = self.request.get('iclickamt')
-    laptop = self.request.get('laptoselect')
+    laptop = self.request.get('laptopselect')
     timestamp = datetime.datetime.now()
     eus = EquipmentUsage(userid=uid,useremail=uemail, equipment=equip, iclickeramt=iclick, laptopsel=laptop,startdate=startdatetime.date(),)
     eus.put()
-    
+    sender_address = "Room Scheduling Notification <notification@roomscheduler490.appspotmail.com>"
+    subject = "Equipment request notification"
+    body = """
+    Your request of: %s, iClicker(s) %s, laptop %s on %s has been submitted.
+    """ % (equip,iclick, laptop, startdatetime.date())
+    mail.send_mail(sender_address, uemail, subject, body)
+    logging.info(body)
+  
     template_args = {
       'equipment': equip,
       'iclicker': iclick,
